5871df2a58356b2eea26e8b06a34992db3d2c19d
sandraparis83-dotcom <sandraparis83@gmail.com>
2026-07-11 16:59:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-
 const boton = document.querySelector("button");
+const circulo = document.getElementById("circulo");
 
 boton.onclick = function () {
-    alert("FUNCIONA");
+    boton.style.display = "none";
+
+    let grande = false;
+
+    setInterval(function () {
+        if (grande) {
+            circulo.style.transform = "scale(1)";
+        } else {
+            circulo.style.transform = "scale(1.4)";
+        }
+
+        grande = !grande;
+    }, 2000);
 };
+
+
+
+    
+
